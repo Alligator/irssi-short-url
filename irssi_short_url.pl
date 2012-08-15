c12ac2bbc18b8b6c1628e325e3e068cfed597543
Alligator <enemy.forest.brigade@gmail.com>
2012-08-15 10:24:52 +0100

    reverted eval thingy. it doesnt work.

--- irssi_short_url.pl
+++ irssi_short_url.pl
@@ -8,7 +8,6 @@ use Irssi::Irc;
 # THINGS WHAT ARE WRONG
 #
 # swallows urls if something goes wrong
-#   um i think it wont now??
 #
 # sometimes fails on realllly crazy urls e.g.
 #   http://books.google.co.uk/books?id=QM3uQqDiAQIC&pg=PA24&dq=%22Bart+vs.+the+Space+Mutan+ts%22&hl=en&ei=Wb87Tr2MHY3u-gbh4oiUAg&sa=X&oi=book_result&ct=result&redir_esc=y#v=onepage&q=%22Bart%20vs.%20the%20Space%20Mutants%22&f=false
@@ -23,46 +22,42 @@ sub getURL {
   my @tokens = split(/ /, $msg);
   my $out;
 
-  eval {
-    foreach (@tokens) {
-      if ($_ =~ /.*(https?:\/\/([^\/]+)[^\s]*)/) {
-        # we got a url
-        my ($url, $domain) = ($1, $2);
+  foreach (@tokens) {
+    if ($_ =~ /.*(https?:\/\/([^\/]+)[^\s]*)/) {
+      # we got a url
+      my ($url, $domain) = ($1, $2);
 
-        # leave short urls be
-        if (length($url) < 30) {
-          $out .= "$_ ";
-          next;
-        }
-
-        my $sock = new IO::Socket::INET(
-          PeerAddr => 'is.gd',
-          PeerPort => 'http(80)',
-          Proto    => 'tcp'
-        ) or die "ERROR in Socket Creation : $!\n";
-
-        my $request = sprintf($isgd_api_request, CGI::escape($url));
-        my $data;
-
-        $sock->send($request);
-        $sock->recv($data, 1024);
-        $sock->close();
-
-        $data =~ /(http.*)\r\n/;
-
-        $out .= "$1 ($domain) ";
-        if (scalar(@lastURLs) > 5) {
-          shift(@lastURLs);
-        }
-        push(@lastURLs, ([$1, $url]));
-      } else {
+      # leave short urls be
+      if (length($url) < 30) {
         $out .= "$_ ";
+        next;
       }
+
+      my $sock = new IO::Socket::INET(
+        PeerAddr => 'is.gd',
+        PeerPort => 'http(80)',
+        Proto    => 'tcp'
+      ) or die "ERROR in Socket Creation : $!\n";
+
+      my $request = sprintf($isgd_api_request, CGI::escape($url));
+      my $data;
+
+      $sock->send($request);
+      $sock->recv($data, 1024);
+      $sock->close();
+
+      $data =~ /(http.*)\r\n/;
+
+      $out .= "$1 ($domain) ";
+      if (scalar(@lastURLs) > 5) {
+        shift(@lastURLs);
+      }
+      push(@lastURLs, ([$1, $url]));
+    } else {
+      $out .= "$_ ";
     }
-    Irssi::signal_continue($server, $out, $nick, $addr, $target);
-  } or do {
-    Irssi::print($@);
   }
+  Irssi::signal_continue($server, $out, $nick, $addr, $target);
 }
 
 # list the last 5 shortened urls
